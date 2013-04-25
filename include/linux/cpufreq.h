ea725d6ca919bae7961e1626af4cf5cb35ae5ce7
carvsdriver <carvsdriver@gmail.com>
2013-04-25 16:00:43 -0400

    cpufreq: default governor typo fix

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -417,7 +417,7 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactive)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASS2)
 extern struct cpufreq_governor cpufreq_gov_smartass2;
-#define CPUFREWQ_DEFAULT_GOVERNOR	(&cpufreq_gov_smartass2)
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_smartass2)
 #endif
 
 
